471a05b3dc285a81acc730340826bec8d61707da
Raquel <rackelrodrigues@yahoo.com.br>
2022-04-27 22:53:26 -0300

    Aula

---
+++ Meus exercicios/Meu primeiro projeto/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Meu_primeiro_projeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1;
+            double nota2;
+            double nota3;
+            double media;
+
+            Console.WriteLine("digite nota 1");
+            Double.TryParse(Console.ReadLine(), out nota1);
+
+            Console.WriteLine("digite nota 2");
+            Double.TryParse(Console.ReadLine(), out nota2);
+
+            Console.WriteLine("digite nota 3");
+            Double.TryParse(Console.ReadLine(), out nota3);
+
+            Console.WriteLine("nota 1 digitada e:");
+            Console.WriteLine(nota1);
+
+            Console.WriteLine("nota 2 digitada e:");
+            Console.WriteLine(nota2);
+
+            Console.WriteLine("nota 3 digitada e:");
+            Console.WriteLine(nota3);
+
+            media = (nota1 + nota2 + nota3) / 3;
+            Console.WriteLine("Sua media e:");
+            Console.WriteLine(media);
+
+            if(media >= 7)
+            { 
+
+        Console.WriteLine("Aprovado");
+            }
+    else
+            { 
+        Console.WriteLine("Reprovado");
+
+
+    }
+
+                
+        }
+    }
+}
---
+++ Meus exercicios/Meusdados/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Meusdados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome;
+            double peso;
+            int idade;
+
+
+            Console.WriteLine("digite seu nome:");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("digite seu peso:");
+            Double.TryParse(Console.ReadLine(), out peso);
+
+            Console.WriteLine("digite sua idade:");
+            int.TryParse(Console.ReadLine(), out idade);
+
+            Console.WriteLine(" seu nome e");
+            Console.WriteLine(nome);
+            Console.WriteLine("seu peso e");
+            Console.WriteLine(peso);
+            Console.WriteLine("sua idade e");
+            Console.WriteLine(idade);
+        }
+
+    }
+}
