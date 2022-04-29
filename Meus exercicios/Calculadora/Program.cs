ccfd18f8b2e9e26c14747b7d99257cac4f12ba38
Raquel <rackelrodrigues@yahoo.com.br>
2022-04-28 22:38:37 -0300

    prova

---
+++ Meus exercicios/Calculadora/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /*desenvolver uma calculadora
+             *que vai ler um numero
+             *depois vai ler o segundo numero
+             *depois vai ler o operador
+             *como resposta o programa
+             *responder o resultado da operação
+             */
+            //ler o primeiro numero
+            //ler o segundo numero
+            //ler o operador + - * /
+            //verificar o tipo do operador
+            //realizar a operação conforme o tipo verificado
+            //imprimir o resultado
+            Double numero1;
+            Double numero2;
+            Double operador;
+            Double resultado;
+
+
+            Console.WriteLine("digite numero1");
+            Double.TryParse(Console.ReadLine(), out numero1);
+            Console.WriteLine("digite o numero2");
+            Double.TryParse(Console.ReadLine(), out numero2);
+            Console.WriteLine("operacao:1 para soma, 2 para subtracao, 3 para multiplicação, 4 para divisao");
+            Double.TryParse(Console.ReadLine(), out operador);
+
+            if (operador == 1)
+            {
+                resultado = numero1 + numero2;
+                Console.WriteLine("o resultado da soma e");
+                Console.WriteLine(resultado);
+
+
+
+
+            }
+            if (operador == 2)
+
+            {
+                resultado = numero1 - numero2;
+                Console.WriteLine("o resultado da subtracao e");
+                Console.WriteLine(resultado);
+
+
+
+            }
+            if (operador == 3)
+
+            {
+                resultado = numero1 * numero2;
+                Console.WriteLine("o resultado da multiplicacao e");
+                Console.WriteLine(resultado);
+
+
+            }
+            if (operador == 4 && numero2 !=0)
+
+            {
+                resultado = numero1 / numero2;
+                Console.WriteLine("o resultado  da divisao e");
+                Console.WriteLine(resultado);
+
+
+
+            }
+            else if (operador == 4)
+            {
+
+
+                if (numero2 == 0)
+                {
+                    Console.WriteLine("impossivel dividir");
+                }
+
+            }  
+
+
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Meus exercicios/Prova/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Prova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escrever um programa que 
+             * vai ler 3 notas de prova
+             * sendo que
+             * a primeira prova tem peso 30%
+             * a segunda prova tem peso de 50%
+             * a terceira prova tem peso 20%
+             * ao final, imprimir a media
+             * e dizer se o akuno foi aprovado
+             * ou nao
+             */
+            const Double prova1 = 0.30;
+            const Double prova2 = 0.50;
+            const Double prova3 = 0.20;
+
+
+            
+            Double nota1, nota2, nota3;
+            Double media;
+
+            Console.WriteLine("digite a nota1");
+            Double.TryParse(Console.ReadLine(), out nota1);
+            Console.WriteLine("digite a nota2");
+            Double.TryParse(Console.ReadLine(), out nota2);
+            Console.WriteLine(" digite a nota3");
+            Double.TryParse(Console.ReadLine(), out nota3);
+            media = (nota1 * prova1 + nota2 * prova2 + nota3 * prova3);
+
+
+
+
+
+
+                
+
+        }
+    }
+}
