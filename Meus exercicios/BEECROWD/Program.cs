8340a26be4936290fe7923675a383addfbf79bce
Raquel <rackelrodrigues@yahoo.com.br>
2022-04-29 22:30:06 -0300

    Beecrowd

---
+++ Meus exercicios/BEECROWD/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BEECROWD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int A, B;
+            int X;
+            int.TryParse(Console.ReadLine(), out A);
+            int.TryParse(Console.ReadLine(), out B);
+
+            X = A + B;
+            Console.WriteLine("X = "+X);
+
+        }
+    }
+}
